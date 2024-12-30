c16c146f513b46556994d5fd1551abf3f2620be6
Edgar Rafael Conrado Hernandez <edgar.conrado@oreillyauto.com>
2024-12-30 15:10:51 -0600

    Circuit Details

--- src/app/race/[id]/index.tsx
+++ src/app/race/[id]/index.tsx
@@ -1,16 +1,50 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams } from 'expo-router';
+import raceReponse from '../../../../assets/data/race.json';
+
+const race = raceReponse.data.races.response[0];
 
 const RaceDetails = () => {
 
   const { id } = useLocalSearchParams();
 
   return (
-    <View>
-      <Text>RaceDetails:{ id }</Text>
+    <View style={ styles.page }>
+      <Text style={styles.circuitSeason}>
+        <Text style={styles.circuitName}>{race.competition.location.country} </Text> 
+        {race.season}
+      </Text>
+      <Text>RaceDetails:{race.circuit.name}</Text>
+
+      <Image
+        source={{ uri: race.circuit.image }}
+        style={styles.image}
+        resizeMode='contain'
+      />
+
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  circuitName: {
+    fontSize: 24,
+    fontFamily: 'F1-Bold'
+  },
+  circuitSeason: {
+    fontSize: 24,
+    fontFamily: 'F1-Regular'
+  },
+  page: {
+    padding: 10,
+
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 16 / 9,
+    marginVertical: 15,
+  }
+});
 
 export default RaceDetails
