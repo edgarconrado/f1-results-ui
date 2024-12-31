cb36ab6bc798ff2c14c94db9a5257e5587d3807c
Edgar Rafael Conrado Hernandez <edgar.conrado@oreillyauto.com>
2024-12-31 10:48:38 -0600

    Race Ranking UI

--- src/app/race/[id]/race.tsx
+++ src/app/race/[id]/race.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
+import raceRakingResponse from '../../../../assets/data/race-rankings.json';
+import RankingListitem from '../../../Components/RankingListItem';
+
+const raceRanking = raceRakingResponse.data.raceRankings.response;
 
 const race = () => {
+
+
   return (
     <View>
-      <Text>race</Text>
+      <FlatList
+        data={raceRanking}
+        renderItem={({item}) => <RankingListitem item={item} />}
+      />
     </View>
   )
 }
